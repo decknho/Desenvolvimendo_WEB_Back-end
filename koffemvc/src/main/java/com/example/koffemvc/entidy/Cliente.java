cb6f8b1c9bd0fe4b468098eb27e0a68eabde31f5
decknho <decknho26@gmail.com>
2026-02-15 13:59:32 -0300

    constructor, toString, hashCode, equals, getters & setter da entidy

--- koffemvc/src/main/java/com/example/koffemvc/entidy/Cliente.java
+++ koffemvc/src/main/java/com/example/koffemvc/entidy/Cliente.java
@@ -1,5 +1,69 @@
 package com.example.koffemvc.entidy;
 
-public class Cliente {
+import java.time.LocalDate;
+import java.util.Objects;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Cliente {
+	private Long id;
+	private String nome;
+	
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	private LocalDate clienteDesde;
+	
+	
+	//constructor
+	public Cliente(long id, String nome, LocalDate clienteDesde) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.clienteDesde = clienteDesde;
+	}
+	
+	
+	//getters & setters
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public LocalDate getClienteDesde() {
+		return clienteDesde;
+	}
+	public void setClienteDesde(LocalDate clienteDesde) {
+		this.clienteDesde = clienteDesde;
+	}
+	
+	
+	//toString
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + ", clienteDesde=" + clienteDesde + "]";
+	}
+	
+	
+	//hashCode & equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(clienteDesde, id, nome);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(clienteDesde, other.clienteDesde) && id == other.id && Objects.equals(nome, other.nome);
+	}
 }
