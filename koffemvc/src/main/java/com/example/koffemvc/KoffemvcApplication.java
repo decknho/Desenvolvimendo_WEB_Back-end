a52c305906f26e1b79ad7ae02cdfe9180490871e
decknho <decknho26@gmail.com>
2026-02-15 07:15:07 -0300

    Estrutura padrão para springBoot

---
+++ koffemvc/src/main/java/com/example/koffemvc/KoffemvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.koffemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoffemvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoffemvcApplication.class, args);
+	}
+
+}
