0fbad524aa70932dec81b8944b5162ee3b804161
decknho <decknho26@gmail.com>
2026-03-18 18:21:14 -0300

    Implement Pedido and Produto controllers, models, and repositories

--- demorest/src/main/java/com/example/demorest/model/Pedido.java
+++ demorest/src/main/java/com/example/demorest/model/Pedido.java
@@ -1,3 +1,76 @@
+package com.example.demorest.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Pedido {
-    
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="cliente_id", nullable=false)
+	private Long clienteId;
+	
+	@Column(name="produto_id", nullable=false)
+	private Long produtoId;
+	
+	@Column(nullable=false)
+	private Integer quantidade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getClienteId() {
+		return clienteId;
+	}
+
+	public void setClienteId(Long clienteId) {
+		this.clienteId = clienteId;
+	}
+
+	public Long getProdutoId() {
+		return produtoId;
+	}
+
+	public void setProdutoId(Long produtoId) {
+		this.produtoId = produtoId;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(clienteId, id, produtoId, quantidade);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		return Objects.equals(clienteId, other.clienteId) && Objects.equals(id, other.id)
+				&& Objects.equals(produtoId, other.produtoId) && Objects.equals(quantidade, other.quantidade);
+	}
 }
